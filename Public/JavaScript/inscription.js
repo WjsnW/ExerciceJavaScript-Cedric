a8eaecfc87bde5f419b3f6b9794af7d0bcc2d1e6
WjsnW <jason57welter@gmail.com>
2021-01-26 16:54:13 +0100

    return inscription

--- Public/JavaScript/inscription.js
+++ Public/JavaScript/inscription.js
@@ -6,11 +6,11 @@ function champValide(ref){
     } else{
         document.getElementById(ref + "F").style.display = "block";
         if (ref == "lieu-n"){
-            document.getElementById(ref + "F").innerHTML = "Vous devez saisir votre lieu de naissance de naissance";
+            document.getElementById(ref + "F").innerHTML = "Vous devez saisir votre lieu de naissance";
         } else if (ref == "signe"){
             document.getElementById(ref + "F").innerHTML = "Vous devez saisir votre signe astrologique";
         } else if (ref == "mdp1" || ref == "mdp2") {
-            document.getElementById(ref + "F").innerHTML = "Vous devez rentre un mots de passe"
+            document.getElementById(ref + "F").innerHTML = "Vous devez rentre un mot de passe"
         } else if (ref == "presen"){
             document.getElementById(ref + "F").innerHTML = "Vous devez saisir une présentation"
         } else {
@@ -21,7 +21,6 @@ function champValide(ref){
 // controle que la date soit dans le bon format
 function dateN(){
     let date = document.getElementById("date").value;
-    console.log("ici");
     if ( date.match(/^[0-3]\d-[0-1]\d-\d{4}$/g)){
         document.getElementById("dateF").style.display = "none";
         return true
@@ -39,13 +38,13 @@ function email(){
     } else{
         document.getElementById("emailF").style.display = "block";
         document.getElementById("emailF").innerHTML = "Veuillez saisir une email correct";
-    }
-}
+    };
+};
 // controle mdp
 function mdp(){
     let mdp1 = document.getElementById("mdp1").value;
     let mdp2 = document.getElementById('mdp2').value;
-    if ( mdp1 == mdp2 ){
+    if ( mdp1 == mdp2 && mdp1.length > 0){
         document.getElementById("mdp1F").style.display = "none";
         document.getElementById("mdp2F").style.display = "none";
         return true
@@ -54,8 +53,8 @@ function mdp(){
         document.getElementById("mdp1F").innerHTML = "Veuillez entrer le même mot de passe";
         document.getElementById("mdp2F").style.display = "block";
         document.getElementById("mdp2F").innerHTML = "Veuillez entrer le même mot de passe";
-    }
-}
+    };
+};
 
 // controle nombre cara
 function nbrCara(){
@@ -65,16 +64,39 @@ function nbrCara(){
     } else{
         document.getElementById("presenF").style.display = "block";
         document.getElementById("presenF").innerHTML = "Veuillez entrer entre 10 et 15 caractére";
-    }
-}
+    };
+};
 // fonction au clique de l'envoi du formulaire
 function envoyer(){
     let a = 0;
-    for (let i of ["nom","prenom","date","lieu-n","email",'mdp1',"mdp2","presen"]){
+    let b = 0;
+    for (let i of ["nom","prenom","lieu-n","email"]){
         if (champValide(i)){
-            console.log("ok");
             a += 1;
         }
     }
-    
+    if (champValide("date") && dateN()){
+        a += 1;
+    }
+    if (email()){
+        a += 1;
+    }
+    if (champValide("mdp1")){
+        b += 1;
+    }
+    if (champValide("mdp2")){
+        b += 1;
+    }
+    if (b == 2){
+        if (mdp()){
+            a += 1;
+        }
+    }
+    if (champValide("presen") && nbrCara()){
+        a += 1;
+    }
+    if (a == 8){
+        window.location = document.location;
+        alert('Votre inscription et valider');
+    }
 };
