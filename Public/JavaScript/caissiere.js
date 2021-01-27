d795575ec6f442214eb70ab724c70c728eb66ff7
WjsnW <jason57welter@gmail.com>
2021-01-27 15:16:24 +0100

    return possibiliter real

--- Public/JavaScript/caissiere.js
+++ Public/JavaScript/caissiere.js
@@ -2,7 +2,7 @@ function calcule() {
     let vend = document.getElementById("vend").value;
     let clien = document.getElementById('client').value;
     let somme;
-    if (vend.match(/^\d{1,}$/g) && clien.match(/^\d{1,}$/g)){
+    if (vend.match(/^[\d.]{1,}$/g) && clien.match(/^[\d.]{1,}$/g)){
         somme = vend - clien;
         if (somme >= 0 ){
             alert("Vous dever rendre : "+somme+" €");
