72de9fbfb2c7be12efc541e308625227e07eb611
WjsnW <jason57welter@gmail.com>
2021-01-27 14:17:34 +0100

    return caissiere

---
+++ Public/JavaScript/caissiere.js
@@ -0,0 +1,15 @@
+function calcule() {
+    let vend = document.getElementById("vend").value;
+    let clien = document.getElementById('client').value;
+    let somme;
+    if (vend.match(/^\d{1,}$/g) && clien.match(/^\d{1,}$/g)){
+        somme = vend - clien;
+        if (somme >= 0 ){
+            alert("Vous dever rendre : "+somme+" €");
+        } else{
+            alert("Le client doit encore donnée : "+ -somme+" €");
+        }
+    } else{
+        alert("veuiller completer tous les champs par des nombre");
+    };
+};
